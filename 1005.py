3e0a2b97f797f00bdf490949edf3baaf6d2079b2
freedom9809 <98573877+freedom9809@users.noreply.github.com>
2022-01-28 15:14:33 +0900

    finish 1010

--- 1005.py
+++ 1005.py
@@ -1,1 +1,13 @@
 from sys import stdin
+T = int(stdin.readline())
+def bridge(A,B):
+    s =1
+    for x in range(1,B+1):
+        s *= A
+        s /= x
+        A-=1
+    return s
+    
+for _ in range(T):
+    N, M = map(int, stdin.readline().split())
+    print(int(bridge(M,N)))
