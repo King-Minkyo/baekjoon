8c3eec7e3481050d9acdb0add33cd32b5d6a23ee
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-04-10 17:04:44 +0900

    [Bronze III] Title: 배수와 약수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,14 @@
+for _ in range(10000):
+    a, b = tuple(map(int, input().split()))
+
+    if a == 0 and b == 0:
+        break
+
+    if b % a == 0:
+        print("factor")
+
+    elif a % b == 0:
+        print("multiple")
+
+    else:
+        print("neither")
